2b11c9f8457fbda20ebf1f790a2c9040fe836940
sp0303 <patnayakunisharat@gmail.com>
2020-06-24 00:02:10 +0530

    Updates

---
+++ precache-manifest.0189c259e7107c07bcd4ce01e379e395.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e19d676f14c0a91ff4bf93f9ab9dd0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d1344bd853accbc3454",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e0cd6ebd4a530993da56",
+    "url": "/static/js/2.7aa721b1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7aa721b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d1344bd853accbc3454",
+    "url": "/static/js/main.e1413fc9.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  },
+  {
+    "revision": "7ad1f30c9e9d1b8f2f6934b423e8626e",
+    "url": "/static/media/person.7ad1f30c.svg"
+  },
+  {
+    "revision": "27c8b60137db6b7ea0dbc678fb995b22",
+    "url": "/static/media/person2.27c8b601.svg"
+  }
+]);
